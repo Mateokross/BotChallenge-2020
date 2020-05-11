2a4480121686f874f90eaa4ab54dee82b6b8e354
Randymagnumm <65089964+Randymagnumm@users.noreply.github.com>
2020-05-11 15:25:53 -0300

    Create Meowth.java

---
+++ Meowth.java
@@ -0,0 +1,96 @@
+package teamrocket;
+import robocode.*;
+import java.awt.*;
+import java.awt.geom.*;
+import robocode.util.*;
+import java.lang.Math;
+
+public class Meowth extends AdvancedRobot
+{
+int a=0,b=0, c = 100; // confianza
+
+//para el radar		
+	double energiaVieja = 100;
+	int direccion = 1;
+	int direcArma;
+//
+
+public void run() {
+	
+	//colores del equipo rocket
+		setBodyColor(new Color(255,255,255));
+		setGunColor(new Color(255,0,0));
+		setRadarColor(new Color(4,0,255));
+		setBulletColor(new Color(164,16,201));
+		setScanColor(new Color(4,0,255));
+		
+	setAdjustGunForRobotTurn(true);// pa desvincular el radar
+	setAdjustRadarForGunTurn(true);// pa desvincular el arma
+	setTurnRadarRight(Double.POSITIVE_INFINITY); // que el radar gire todo el tiempo
+	
+		
+	while(true) {
+		back(50); // aca ponemos el movimiento
+		setTurnRadarRight(Double.POSITIVE_INFINITY);} // esto es por si lo pierde que lo vuelva a encontrar
+	}
+public void onBulletMissed(BulletMissedEvent e) {
+	a = Math.max(c-10,5);
+	c=a;
+}	
+public void onBulletHit(BulletHitEvent e) {
+	b = Math.min(c+30,100);
+	c=b;
+	}
+
+public void onScannedRobot(ScannedRobotEvent e) {
+	
+	//estar perpendicular al enemigo
+	setTurnRight(e.getBearing()+90-30*direccion);
+	double cambioEnergia = energiaVieja-e.getEnergy(); //variables que vamos a usar para saber si disparo
+	
+	//esquivas la bala
+	if (cambioEnergia > 0 && cambioEnergia<=3){ //el maximo es 3 para asi no se confunde con eventos que le bajan vida
+		direccion= direccion*-1; //esquivamos la bala
+		setAhead((e.getDistance()/4+25)*direccion); //distancia que nos corremos para esquivar
+	}//si estamos mas cerca nos corremos menos
+	
+	//por si lo habiamos perdido de vista
+	direcArma = direcArma*-1;
+	setTurnGunRight(Double.POSITIVE_INFINITY*direcArma);
+	energiaVieja = e.getEnergy();
+//	
+
+	double PotenciaMax = Math.min(3.0,getEnergy()); // no nos morimos disparando
+	double Potencia = Math.min(PotenciaMax, e.getEnergy()); // no gastamos al pedo
+	double mix = getX();
+	double miy = getY();
+	double pb = getHeadingRadians() + e.getBearingRadians(); // bearing pero solo +
+	double ex = getX() + e.getDistance() * Math.sin(pb);
+	double ey = getY() + e.getDistance() * Math.cos(pb); // donde esta el tipo
+	double ehead = e.getHeadingRadians(); // adonde apunta
+	double evel = e.getVelocity(); // su velocidad
+	double alto = getBattleFieldHeight(),ancho = getBattleFieldWidth(); // la cancha
+	double pex = ex, pey = ey; // donde va a estar, arranco con esto y le sumo su mov
+	double t = 0; // esto es para ver cuanto tiempo queda en el tick
+	double velB = 20 - Potencia*3; //esta es la velocidad de la bala
+	
+	while ((++t) * (velB) < Point2D.Double.distance(mix, miy, pex, pey)){
+	// mientras la distancia que puede hacer la bala este turno es menor q la que efectivamente hay
+	pex += Math.sin(ehead) * evel; // Entonces, le calculo donde va a estar el enemy
+	pey += Math.cos(ehead) * evel; // a partir de donde va y su vel
+	if (pex < 18 || pey < 18 || pex > ancho - 18 || pey > alto - 18)
+	{ // esto dice que si el bot se la esta por dar contra la pared no disparar como si siguiese, 18 es la h y w
+		pex = Math.min(Math.max(18.0, pex), ancho - 18.0);	
+		pey = Math.min(Math.max(18.0, pey), alto - 18.0);
+		break;
+	}
+}
+double alfa = Utils.normalAbsoluteAngle(Math.atan2(pex - getX(), pey - getY()));
+// math.atan2 me dice el angulo que forma el eje x con la linea que une los puntos donde estoy y donde va a estar el gil
+// normalabsoluteangle lo pasa a entre 0 y 2pi xq el de arriba va de menos pi a mas pi
+setTurnRadarRightRadians(Utils.normalRelativeAngle(pb - getRadarHeadingRadians())); 
+// apunto a donde va a estar al final de t, relativo a donde yo estoy apuntando ahora
+setTurnGunRightRadians(Utils.normalRelativeAngle(alfa - getGunHeadingRadians())); // apunto a donde va a estar cuando le llegue la bala
+fire(Potencia*c/100); //disparo
+
+}}
